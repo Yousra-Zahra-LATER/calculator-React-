23ad0761aac5b442c278d13d62e97184bd0534c1
Yousra-Zahra-LATER <yousra.later@univ-constantine2.dz>
2023-09-08 03:22:39 +0200

    Delete functionCal.js

--- src/functionCal.js
+++
@@ -1,8 +0,0 @@
-import { ACTIONS } from "./App";
-import { useReducer } from 'react';
-  //fonction Dispatch indique au reducer que vous souhaitez effectuer une action de type "ADD_DIGIT" 
-  //et que vous lui fournissez la valeur du chiffre que vous souhaitez ajouter 
- const handleDigitClick = (digit) => {
-    dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } });
-  };
-  export default handleDigitClick;
